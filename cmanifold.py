b8c7c66caa9e63062ebc62bc07655b27a8c98d94
bpillet <bpillet@users.noreply.github.com>
2015-03-02 16:57:40 +0100

    almost complex

--- cmanifold.py
+++ cmanifold.py
@@ -1,5 +1,6 @@
 from sage.structure.unique_representation import UniqueRepresentation
 from domain import OpenDomain
+from manifold import Manifold
 
 class ComplexManifold(OpenDomain):
   def __init__(self, dim, name, latexname=None, start_index=0):
@@ -18,10 +19,21 @@ class ComplexManifold(OpenDomain):
     M=Manifold(2*n, 'M')
     M.domains = self.domains
     return M
-    
-    
 
+class AlmostComplexManifold(Manifold):
+  def __init__(self, M, I):
+    self.manifold = M
+    if not I.type() == (1,1):
+	raise TypeError("The almost-complex structure must be of type (1,1)")
+    if not I.contract(I,1):
+	raise TypeError("The tensor of structure must square to minus identity")
+    self.acstruct = I
 
+  def nijenhuis_tensor(self):
+    return self.acstruct
+  def is_integrable(self):
+    return True
+    
 
 class ComplexLineManifold(ComplexManifold, UniqueRepresentation):
 
