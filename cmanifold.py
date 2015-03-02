268246bf0623a7be97a7cdc8c982ad2d98a55749
Basile Pillet <bpillet@users.noreply.github.com>
2015-03-02 12:58:12 +0100

    Update cmanifold.py

--- cmanifold.py
+++ cmanifold.py
@@ -1,10 +1,23 @@
-class: ComplexManifold(OpenDomain):
+from sage.structure.unique_representation import UniqueRepresentation
+from domain import OpenDomain
+
+class ComplexManifold(OpenDomain):
   def __init__(self, dim, name, latexname=None, start_index=0):
+    if not isinstance(n, (int, Integer)):
+        raise TypeError("The manifold dimension must be an integer.")
+    if n<1:
+        raise ValueError("The manifold dimension must be strictly " + "positive.")
+    self.dim = n
+    OpenDomain.__init__(self, self, name, latex_name)
+    self.sindex = start_index
+    self.domains = {self.name: self}
   def _repr_(self):
   def
   def forget(self):
     n=self.dimension()
-    M=Manifold(2*n, )
+    M=Manifold(2*n, 'M')
+    M.domains = self.domains
+    return M
     
     
 
@@ -19,7 +32,7 @@ class ComplexLineManifold(ComplexManifold, UniqueRepresentation):
   def _repr_(self):
     return "field C of complex numbers"
 
-class: ComplexPlaneManifold(Manifold, UniqueRepresentation):
+class ComplexPlaneManifold(Manifold, UniqueRepresentation):
 
   def __init__(self):
     from chart import Chart
