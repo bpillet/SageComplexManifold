9aeae76211e13170159f674ede38fc73de84eaab
Basile Pillet <bpillet@users.noreply.github.com>
2015-03-08 12:32:31 +0100

    Update quaternion.py multiplication

--- quaternion.py
+++ quaternion.py
@@ -4,21 +4,24 @@ class Quaternion:
 		self.xi = b
 		self.xj = c
 		self.xk = d
-		self.v = a + i*b
-		self.w = c + i*d
 	def __repr__(self):
 		print("{0} + {1}i + {2}j + {3}k", self.x1, self.xi, self.xj, self.xk)
 	def forme_complexe(self):
-		print("{0} + {1}j", self.v, self.w)
+		print("{0} + {1}j", self.x1 + i*self.xi, self.xj+ i*self.xk)
 	def __add__(self, autre):
-		q = self
+		q = Quaternion(0,0,0,0)
 		q.x1 = self.x1 + autre.x1
 		q.xi = self.xi + autre.xi
 		q.xj = self.xj + autre.xj
 		q.xk = self.xk + autre.xk
-		q.v = self.v + autre.v
-		q.w = self.w + autre.w
 		return q
-	def __sub__(self,autre):
+	def __mul__(self, autre):
+		q = Quaternion(0,0,0,0)
+		q.x1 = self.x1 * autre.x1 - self.xi * autre.xi - self.xj * autre.xj - self.xk * autre.xk
+		q.xi = self.x1 * autre.xi + self.xi * autre.x1 + self.xj * autre.xk - self.xk * autre.xj
+		q.xj = 
+		q.xk = 
+		return q
+	def __sub__(self, autre):
 		moins1 = Quaternion(-1,0,0,0)
 		return self.__add__(autre.__mul__(moins1))
