192c64237dc3c94e505bd1eca61019cbc6792263
bpillet <bpillet@users.noreply.github.com>
2015-03-10 10:23:36 +0100

    Almost complex dbarre + mineurs

--- src/sage/geometry/manifolds/acmanifold.py
+++ src/sage/geometry/manifolds/acmanifold.py
@@ -5,10 +5,12 @@ from manifold import Manifold
 class AlmostComplexManifold(Manifold):
 	def __init__(self, M, I):
 		Manifold.__init__(self, M.dim, M.name, M.latex_name, M.index)
-    		if not I.type() == (1,1):
-			raise TypeError("The almost-complex structure must be of type (1,1)")
+		if not I.domain() == M:
+			raise TypeError("The tensor of almost-complex structure must be defined on the whole manifold " + M)
+    		if not I.tensor_type() == (1,1):
+			raise TypeError("The tensor of almost-complex structure must be of tensor type (1,1)")
 		if not I.contract(I) + M.tangent_identity_field() == 0:
-			raise TypeError("The tensor of structure must square to minus identity")
+			raise TypeError("The tensor of almost-complex structure must square to minus identity")
 		self.almost_complex_structure = I
 
 	def _repr_(self):
@@ -21,22 +23,49 @@ class AlmostComplexManifold(Manifold):
 	        return Manifold._repr_(M) + description
 
 	def almost_complex_structure(self):
+        	r"""
+        	Returns the tensor of almost-complex structure.
+        	"""
     		return self.almost_complex_structure
 
   	def nijenhuis_tensor(self):
 		I = self.almost_complex_structure
 		return "ONGOING"
 
-	def dbarre(self,T):
-		return "ONGOING"
+	def dbarre(self,tau):
+        	r"""
+        	Returns the antiholormophic part of the exterior derivative of a differential form.
+                
+		INPUT:
+		- ``tau`` -- a complex valued differential n-form of complex type (p,q) on the manifold (or an open domain ??)
+		
+		OUTPUT:
+		- a complex valued differential (n+1)-form on the manifold of complex type (p,q+1)
+        	"""
+		dtau = tau.exterior_der()
+		return (self.tangent_identity_field() + i*I).contract(dtau)
+
+	def drond(self,tau):
+        	r"""
+        	Returns the antiholormophic part of the exterior derivative of a differential form.
+                
+		INPUT:
+		- ``tau`` -- a complex valued differential n-form of complex type (p,q) on the manifold (or an open domain ??)
+		
+		OUTPUT:
+		- a complex valued differential (n+1)-form of complex type (p,q+1) on the manifold
+        	"""
+		dtau = tau.exterior_der()
+		return (self.tangent_identity_field() - i*I).contract(dtau)
 
 	def is_integrable(self):
 		if self.nijenhuis_tensor() == 0:
 			return True
 		return False
 
+### The Complex Plane as an subclass of AlmostComplexManifold
 
-class ComplexPlaneManifold(AlmostComplexManifold, UniqueRepresentation):
+class ComplexPlaneManifold(AlmostComplexManifold):
 
 	def __init__(self):
 		from chart import Chart
